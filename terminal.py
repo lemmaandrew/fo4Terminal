786d5bc221f05634f3edcc3c2e38152e5118d1bd
TuttleStripes <46587239+TuttleStripes@users.noreply.github.com>
2020-04-07 22:04:34 -0500

    Updates algorithm to use word bank

--- terminal.py
+++ terminal.py
@@ -1,17 +1,23 @@
-from collections import defaultdict
-from PIL import Image, ImageEnhance, ImageFilter, ImageGrab, ImageOps
+"""Solves the hacking mini-game in Fallout 4"""
+import json
+import re
+import warnings
+from collections import defaultdict\
+
 import numpy as np
 import pyautogui
 import pytesseract
-import re
-import time
-import warnings
+from PIL import Image, ImageEnhance, ImageFilter, ImageGrab, ImageOps
 
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 warnings.filterwarnings('ignore')
 
-def locateCarrot(img: Image) -> tuple:
+with open('words.json') as f:
+    WORDS = json.load(f)
+
+
+def locate_carrot(img: Image) -> tuple:
     """Locates the '>' that is next to words in the input field of the terminal"""
     allcarrots = list(pyautogui.locateAll(Image.open('carrotonly.png'), img, confidence=0.75))
     carrot = max(allcarrots, key=lambda x: x.left + x.top)
@@ -21,36 +27,34 @@ def locateCarrot(img: Image) -> tuple:
         carrot.top + carrot.height + 5)
 
 
-def captureCarrot(img: Image) -> Image:
-    """Creates an image with the screen region of locateCarrot"""
+def capture_carrot(img: Image) -> Image:
+    """Creates an image with the screen region of locate_carrot"""
     imgarr = np.array(img, dtype=np.uint8)
-    carrot = list(locateCarrot(img))
+    carrot = list(locate_carrot(img))
     return Image.fromarray(imgarr[carrot[1]:carrot[3], carrot[0]:carrot[2]])
 
 
-def fixScreen(img: Image) -> Image:
+def fix_screen(img: Image, tolerance: int=10) -> Image:
     """Prepares an image for reading with pytesseract"""
+    img = img.convert('L')
     med = img.filter(ImageFilter.MedianFilter())
     enhanced = ImageEnhance.Contrast(med).enhance(2)
     imgarr = np.array(enhanced, dtype=np.uint8)
-    #imgarr[imgarr < 70] = 0
-    mask0 = np.less(np.divide(imgarr[:, :, 1], imgarr[:, :, 0]), 20)
-    mask2 = np.less(np.divide(imgarr[:, :, 1], imgarr[:, :, 2]), 20)
-    imgarr[mask0 & mask2] = 0
-    #not black -> white
-    #imgarr[np.not_equal.reduce(imgarr != 0, axis=-1)] = 255
-    return Image.fromarray(imgarr).convert('LA')
+    imgarr[np.all(imgarr < tolerance, axis=-1)] = 0
+    imgarr[np.all(imgarr >= tolerance, axis=-1)] = 255
+    img = Image.fromarray(imgarr)
+    return ImageOps.invert(ImageEnhance.Contrast(img).enhance(2))
 
 
-def wordSearch(region, wordlen: int) -> set:
+def word_search(region, wordlen: int) -> set:
     """Mouse over each word in the terminal while this is active then press Ctrl-C in the windows terminal to return the found words"""
     words = set()
     try:
         while True:
             try:
-                txt = pytesseract.image_to_string(fixScreen(ImageGrab.grab(region)), lang='eng')
+                txt = pytesseract.image_to_string(fix_screen(ImageGrab.grab(region)), lang='eng')
                 txt = re.match(r'[a-zA-Z]{' + str(wordlen) + '}', txt)[0].upper()
-                if len(txt) != wordlen or txt in words:
+                if txt in words or txt not in WORDS[str(wordlen)]:
                     continue
                 print(txt)
                 words.add(txt)
@@ -62,23 +66,22 @@ def wordSearch(region, wordlen: int) -> set:
 
 def likeness(word1: str, word2: str) -> int:
     """returns how many times each word has the same letter at each index"""
-        s = 0
-        for i in range(len(word1)):
-            s += word1[i] == word2[i]
-        return s
+    s = 0
+    for idx, letter in enumerate(word1):
+        s += letter == word2[idx]
+    return s
 
 
-def suggestClick(words: set) -> str:
+def suggest_click(words: set) -> str:
     """suggests which word to click based on the lowest average likeness between words"""
-    def likenessChart(word):
+    def likeness_chart(word):
         chart = defaultdict(int)
         for i in range(len(word)):
             for j in words:
                 chart[i] += likeness(word, j) == i
         return np.average(list(chart.values()))
-    
-    minw = min(words, key=likenessChart)
-    print(f'Click on: {minw}, score: {likenessChart(minw)}')
+    minw = min(words, key=likeness_chart)
+    print(f'Click on: {minw}, score: {likeness_chart(minw)}')
     return minw
 
 
@@ -91,13 +94,19 @@ def guess(words: set, word: str, similarity: int) -> None:
 
 
 if __name__ == "__main__":
-    input("MAKE SURE YOU'RE IN THE TERMINAL SCREEN AND THAT '>' ISN'T SELECTED")
-    wordlength = int(input('Length of words: '))
-    print('Starting...            ')
-    print('Press Ctrl-C when all words have been cached')
-    box = locateCarrot(ImageGrab.grab((0, 0, 1920, 1080)))
-    cache = wordSearch(box, wordlength)
     while True:
-        clickhere = suggestClick(cache)
-        sim = int(input(f'What is the likeness of "{clickhere}"? '))
-        guess(cache, clickhere, sim)
+        input("MAKE SURE YOU'RE IN THE TERMINAL SCREEN AND THAT '>' ISN'T SELECTED")
+        wordlength = int(input('Length of words: '))
+        print('Starting...            ')
+        print('Press Ctrl-C when all words have been cached')
+        box = locate_carrot(ImageGrab.grab((0, 0, 1920, 1080)))
+        cache = word_search(box, wordlength)
+        while True:
+            clickhere = suggest_click(cache)
+            try:
+                sim = input(f'What is the likeness of "{clickhere}"? ("done" to finish): ')
+                guess(cache, clickhere, int(sim))
+            except ValueError as e:
+                if sim == 'done':
+                    print('\n\n\n')
+                    break
